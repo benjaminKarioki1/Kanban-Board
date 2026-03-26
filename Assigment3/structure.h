b503a396ac7859c5229dd797e6c1d2fa68996bca
Jiahong Liu <jiahong.liu@ucdconnect.ie>
2026-03-26 17:28:55 +0000

    update stuct

---
+++ Assigment3/structure.h
@@ -0,0 +1,17 @@
+typedef struct Boards
+{
+	//name of the list
+	char name[30];
+	//pointer to next element
+	struct Elements* nextE;
+	//pointer to next list
+	struct Boards* nextL;
+}Board;
+ 
+typedef struct Elements 
+{
+	//name of the element
+	char name[30];
+	//pointer to next element
+	struct Elements* nextE;
+}Element;
