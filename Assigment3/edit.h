dca8504b89f1a66c9e98e26229ed8e934b8818a1
Benjamin Karioki <benjamin.karioki@ucdconnect.ie>
2026-03-28 21:23:24 +0000

    update

--- Assigment3/edit.h
+++ Assigment3/edit.h
@@ -2,4 +2,7 @@
 
 Listptr findList(Listptr head, char *targetName);
 Elementptr findElement(Elementptr head, char *targetName);
-void renameItem(Listptr head);
+
+void renameItem(Listptr foundList);
+void addItem(Listptr foundList);
+void deleteItem(Listptr foundList);
