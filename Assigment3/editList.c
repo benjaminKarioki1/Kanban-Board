8670d3f37984808438c97ad5dbf6c5aa8ca05769
Benjamin Karioki <benjamin.karioki@ucdconnect.ie>
2026-04-02 02:37:13 +0100

    update

--- Assigment3/editList.c
+++ Assigment3/editList.c
@@ -32,31 +32,45 @@ Elementptr findElement(Elementptr head, char* targetName) {
 }
 
 // function to rename the element of a board
-void renameItem(Listptr foundElement) {
+void renameItem(Listptr foundList) {
+    // ask which element to rename
+    char itemName[30];
+    printf("Enter the name of the item to edit: ");
+    fgets(itemName, sizeof(itemName), stdin);
+    itemName[strcspn(itemName, "\n")] = '\0';
 
+    // find the element inside the list
+    Elementptr foundElement = findElement(foundList->head, itemName);
+
+    // check if it exists
     if (foundElement == NULL) {
-        printf("Can't find item");
+        printf("Can't find item\n");
         return;
     }
-    else {
-        char newName[30];
-        printf("Enter new name for the item");
-        fgets(newName, sizeof(newName), stdin);
-        newName[strcspn(newName, "\n")] = '\0';
-        strcpy(foundElement->name, newName);
-    }
+
+    // ask for the new name
+    char newName[30];
+    printf("Enter new name for item '%s': ", itemName);
+    fgets(newName, sizeof(newName), stdin);
+    newName[strcspn(newName, "\n")] = '\0';
+
+    //  overwrite the old name
+    strcpy(foundElement->name, newName);
 }
 
 //function to add an element to the board
 void addItem(Listptr foundList) {
+    //allocate memory for a new node
     Elementptr newElement = malloc(sizeof(Node));
     if (newElement == NULL) { printf("Error: No Memory\n"); return; }
 
+    //get the name of the new item
     printf("Enter the name of the new item: ");
     char itemName[20];
     fgets(itemName, sizeof(itemName), stdin);
     itemName[strcspn(itemName, "\n")] = '\0';
 
+    //add it to the list and set the pointers to the next node
     strcpy(newElement->name, itemName);
     newElement->nextE = foundList->head;
     foundList->head = newElement;
@@ -64,6 +78,7 @@ void addItem(Listptr foundList) {
 
 //function to delete the element of a board
 void deleteItem(Listptr foundList) {
+    //ask which element to delete
     char name[20];
     printf("Enter the name of the item to delete: ");
 	fgets(name, sizeof(name),stdin);
@@ -72,13 +87,15 @@ void deleteItem(Listptr foundList) {
     Elementptr current = foundList->head;
     Elementptr prev = NULL;
 
+    //check if the list is empty
     if (foundList->head == NULL)
     {
         printf("The list is empty");
         return;
     }
     else 
-    {
+    {   
+        //otherwise check if the name the user gave is the same as the head node
         if (strcmp(foundList->head->name, name)== 0) 
         {
             Elementptr temp = foundList->head; //save old head node
@@ -87,8 +104,10 @@ void deleteItem(Listptr foundList) {
         } 
         else
         {
+            
             while (current != NULL)
             {
+                //check equivalence for the rest of the nodes
                 if (strcmp(current->name, name) == 0){
                     prev->nextE = current->nextE; //moves "prev" pointer to the value of nextE pointed to by "current"
                     free(current); //free memory of the current node
